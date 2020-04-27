447b733112f22d030739de62d20b47d81bbb67c1
HuyCam <camhuy165@gmail.com>
2020-04-27 16:42:25 -0500

    init

---
+++ EditContactCommand.java
@@ -0,0 +1,23 @@
+package com.example.sharingapp;
+
+import android.content.Context;
+
+public class EditContactCommand extends Command {
+    private ContactList contact_list;
+    private Contact oldContact;
+    private Contact newContact;
+    private Context context;
+
+    public EditContactCommand(ContactList contact_list, Contact oldContact, Contact newContact, Context context) {
+        this.contact_list = contact_list;
+        this.oldContact= oldContact;
+        this.newContact = newContact;
+        this.context = context;
+    }
+
+    public void execute() {
+        contact_list.deleteContact(oldContact);
+        contact_list.addContact(newContact);
+        setIsExecuted(contact_list.saveContacts(context));
+    }
+}
